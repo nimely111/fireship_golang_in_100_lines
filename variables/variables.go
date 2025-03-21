01bb15e0db6213cbd9dfbcb12ee402b20e1ef03b
Nimely111 <samuelehnimely@gmail.com>
2025-03-21 11:07:51 +0000

    learning about variables in golang

---
+++ variables/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// Declare a single variable
+
+var a int
+
+// Declare a group of variables
+
+var (
+	b bool
+	c float32
+	d string
+)
+
+func main() {
+	a = 42
+	b, c = true, 3.14
+	d = "Hello"
+	fmt.Println("a:", a, " b:", b, " c:", c, " d:", d)
+}
