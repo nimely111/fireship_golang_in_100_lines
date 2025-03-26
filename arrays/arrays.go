3f672802ae68bc8f576630752f3d64cc8fa1d765
Nimely111 <samuelehnimely@gmail.com>
2025-03-26 20:33:12 +0000

    changing a value by its index to a new value

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -6,6 +6,7 @@ func main() {
 	// var ages [3]int = [3]int{20, 25, 30}
 	var ages = [3]int{20, 25, 30}
 	names := [4]string{"James", "Esther", "Peter", "Mary"}
+	names[1] = "Vick"
 	fmt.Println(names, len(names))
 	fmt.Println(ages, len(ages))
 }
