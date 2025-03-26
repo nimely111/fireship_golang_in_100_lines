636dace316622046cc57b07d98a0dc0958a7537c
Nimely111 <samuelehnimely@gmail.com>
2025-03-26 18:16:17 +0000

    using types in go

--- types/types.go
+++ types/types.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println()
+	/*user specified types */
+	const a int32 = 12        // 32-bit integer
+	const b float32 = 20.5    //  32-bit float
+	var c complex128 = 1 + 4i //  128-bit complex number
+	var d uint16 = 14         //   16-bit unsigned integer
+
+	/* default types */
+	n := 42              // int
+	pi := 3.14           // float64
+	x, y := true, false  // bool
+	z := "Go is awesome" // string
+
+	fmt.Printf("user-specified types: %T %T %T %T \n", a, b, c, d)
+	fmt.Printf("default types: %T %T %T %T %T", n, pi, x, y, z)
 }
