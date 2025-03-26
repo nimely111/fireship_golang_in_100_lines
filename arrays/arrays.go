682b4acada206bb2ac55c1cb22af7d109abbd50b
Nimely111 <samuelehnimely@gmail.com>
2025-03-26 20:28:40 +0000

    using the shorthand assignment operator for the strings

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -5,5 +5,7 @@ import "fmt"
 func main() {
 	// var ages [3]int = [3]int{20, 25, 30}
 	var ages = [3]int{20, 25, 30}
+	names := [4]string{"James", "Esther", "Peter", "Mary"}
+	fmt.Println(names, len(names))
 	fmt.Println(ages, len(ages))
 }
