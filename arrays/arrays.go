4425db55467029fa8a6b512e0d76f3e81ab54d74
Nimely111 <samuelehnimely@gmail.com>
2025-03-28 20:05:50 +0000

    looping over an array

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -4,23 +4,32 @@ import "fmt"
 
 func main() {
 	// var ages [3]int = [3]int{20, 25, 30}
-	var ages = [3]int{20, 25, 30}
-	names := [4]string{"James", "Esther", "Peter", "Mary"}
-	names[1] = "Vick"
-	fmt.Println(names, len(names))
-	fmt.Println(ages, len(ages))
+	// var ages = [3]int{20, 25, 30}
+	// names := [4]string{"James", "Esther", "Peter", "Mary"}
+	// names[1] = "Vick"
+	// fmt.Println(names, len(names))
+	// fmt.Println(ages, len(ages))
 
-	// slices (uses array under the hood)
-	var scores = []int{100, 50, 60}
-	scores[2] = 25
-	scores = append(scores, 45)
-	fmt.Println(scores, len(scores))
+	// // slices (uses array under the hood)
+	// var scores = []int{100, 50, 60}
+	// scores[2] = 25
+	// scores = append(scores, 45)
+	// fmt.Println(scores, len(scores))
 
-	// slice ranges
-	rangeOne := names[1:3]
-	rangeTwo := names[2:]
-	rangeThree := names[:3]
-	rangeOne = append(rangeOne, "Jestina")
-	fmt.Println(rangeOne, rangeTwo, rangeThree)
+	// // slice ranges
+	// rangeOne := names[1:3]
+	// rangeTwo := names[2:]
+	// rangeThree := names[:3]
+	// rangeOne = append(rangeOne, "Jestina")
+	// fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	/* Define an array of size 4 that stores deployment options */
+	var DeploymentOptions = [4]string{"R-pi", "AWS", "GCP", "Azure"}
+
+	/* Loop through the deployment options array */
+	for i := 0; i < len(DeploymentOptions); i++ {
+		option := DeploymentOptions[i]
+		fmt.Println(i, option)
+	}
 
 }
