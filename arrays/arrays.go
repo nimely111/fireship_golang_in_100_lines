d267800787b127ead2e76ead9424d5996bea05bd
Nimely111 <samuelehnimely@gmail.com>
2025-03-30 22:26:26 +0000

    keepimg the code clean and avoiding unused variable errors

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -36,8 +36,8 @@ func main() {
 	DeploymentOptions := [...]string{"R-pi", "AWS", "GCP", "Azure"}
 
 	/* Loop through the deployment options array */
-	for index, option := range DeploymentOptions {
-		fmt.Println(index, option)
+	for _, option := range DeploymentOptions {
+		fmt.Println(option)
 	}
 
 }
